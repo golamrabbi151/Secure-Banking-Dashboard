76ebe026f192f422a1d8b51d017c7885498e1d77
golamrabbi151 <rabbi15-242@diu.edu.bd>
2021-03-11 01:56:52 +0600

    initial commit

---
+++ src/pages/admin/client/Create.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.scss'
+
+const Create = () => {
+    return (
+        <div className="container-fluid py-5">
+            <div className="row">
+                <div className="col-12 ">
+                    <h1>Create Client</h1>
+                    <p>Create form !!!</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Create;
