c8ea56877408762ce1bd2c72e3a2c12282a9d565
golamrabbi151 <rabbi15-242@diu.edu.bd>
2021-03-12 00:55:40 +0600

    client create form design

--- src/pages/admin/client/Create.js
+++ src/pages/admin/client/Create.js
@@ -4,10 +4,55 @@ import './style.scss'
 const Create = () => {
     return (
         <div className="container-fluid py-5">
-            <div className="row">
-                <div className="col-12 ">
-                    <h1>Create Client</h1>
-                    <p>Create form !!!</p>
+            <div className="shadow" style={{ marginLeft: 15,marginRight: 15, backgroundColor: '#fff', paddingTop: 50, paddingBottom: 50 }}>
+                <div className="row" >
+                    <div className="col-12 col-lg-8 offset-lg-2 create-col-style" >
+                        <h3 className="text-center ">Create Client</h3>
+                        <form>
+                            <div className="mb-3">
+                                <label for="Name" className="form-label">Client Name</label>
+                                <input type="text" className="form-control" id="Name" />
+                            </div>
+                            <div className="mb-3">
+                                <label for="Email" className="form-label">Email</label>
+                                <input type="text" className="form-control" id="Email" />
+                            </div>
+                            <div class="row">
+                                <div class="col">
+                                <div className="mb-3">
+                                <label for="DateOfBirth" className="form-label">Date of Birth</label>
+                                    <input type="date" class="form-control" id="DateOfBirth"  />
+                                </div>
+                                </div>
+                                <div class="col">
+                                    <div className="mb-3">
+                                <label for="AccountNumber" className="form-label">Account Number</label>
+                                    <input type="number" id="AccountNumber" class="form-control"   />
+                                </div>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="col">
+                                <div className="mb-3">
+                                <label for="BankName" className="form-label">Bank Name</label>
+                                    <input type="text" class="form-control" id="BankName"  />
+                                </div>
+                                </div>
+                                <div class="col">
+                                <div className="mb-3">
+                                <label for="Branch" className="form-label">Branch</label>
+                                    <input type="text" id="Branch" class="form-control"   />
+                                </div>
+                                </div>
+                            </div>
+                            <button type="sumbit" class="btn shadow-none" style={{backgroundColor:'#ff5733',color:'#fff'}}>Create</button>
+                            
+
+
+                        </form>
+
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/admin/users/Index.js
+++ src/pages/admin/users/Index.js
@@ -25,7 +25,6 @@ const Index = () => {
         }
     }, [])
 
-
     useEffect(() => {
         getItems()
     }, [getItems])
